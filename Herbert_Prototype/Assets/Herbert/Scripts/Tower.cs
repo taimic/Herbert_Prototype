692eaebd493c62ecdc502bd97454871989f0acd2
ScarletLeFairy <scarletlefairy@yahoo.de>
2016-07-04 16:04:34 +0200

    no message

--- Herbert_Prototype/Assets/Herbert/Scripts/Tower.cs
+++ Herbert_Prototype/Assets/Herbert/Scripts/Tower.cs
@@ -1,21 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System; 
+using System;
+using System.Collections.Generic;
 
-public class Tower : Component
+public class Tower : MonoBehaviour
 {
-	Attach attach = null;
+	private Attach attach = null;
 
-	private int Energy;
+	private int Energy = 100;
 	private bool active = false;
 
+    private List<GameObject> targets; 
 
-	public Tower ()
-	{
-			
-	}
+
+	void Start()
+    {
+        targets = new List<GameObject>();
+
+
+        gameObject.AddComponent<Laser>();
+    }
+
+    void Update()
+    {
+        if (targets.Count > 0)
+        {
+            // TODO
+        }
+
+    }
+
 
 	public void Activate() { active = true; }
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        // if (other.gameObject.CompareTag("Ship"))
+        if (!other.gameObject.CompareTag("Tower"))
+        {
+            Debug.Log(other.gameObject + " entered the trigger");
+
+            //TODO add targets
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        // if (other.gameObject.CompareTag("Ship"))
+        if (!other.gameObject.CompareTag("Tower"))
+        {
+            Debug.Log(other.gameObject + " is within the trigger");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // if (other.gameObject.CompareTag("Ship"))
+        if (!other.gameObject.CompareTag("Tower"))
+        {
+            Debug.Log(other.gameObject + " left the trigger");
+
+            //TODO delete targets
+        }
+    }
 
 
 
