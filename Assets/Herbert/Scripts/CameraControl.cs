973d12ab0164ba1eb6f0fd737debdcc95d468a45
ad0bert <pe.ortner@gmail.com>
2016-07-06 16:16:02 +0200

    added cam resize

--- Assets/Herbert/Scripts/CameraControl.cs
+++ Assets/Herbert/Scripts/CameraControl.cs
@@ -3,50 +3,39 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-    Ship[] ships;
-    
-	// Use this for initialization
-	void Start () {
-        ships = FindObjectsOfType<Ship>();
+    private Camera cam;
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
 
+    // Update is called once per frame
+    void Update()
+    {
+        Ship[] ships = FindObjectsOfType<Ship>();
         if (ships.Length == 0) return;
 
-        Vector3 center = Vector3.zero;
-        
+        Vector3 center = new Vector3();
+        float max = float.MinValue;
+        float min = float.MaxValue;
         foreach (Ship s in ships)
         {
+            if (max < s.transform.position.x)
+                max = s.transform.position.x;
+            if (min > s.transform.position.x)
+                min = s.transform.position.x;
+
             center += s.transform.position;
         }
-
+      
         center /= ships.Length;
-        center.z = transform.position.z;
-        transform.position = center;
+        cam.transform.position = new Vector3(center.x, center.y, cam.transform.position.z);
 
-        float max = 0f;
-        Vector3 origin = Vector3.zero;
-
-        foreach (Ship s in ships)
-        {
-            if (Vector3.Distance(center, s.transform.position) > max)
-            {
-                max = Vector3.Distance(center, s.transform.position);
-                origin = s.transform.position;
-            }
-        }
-
-
-        Vector3 viewP = gameObject.GetComponent<Camera>().WorldToViewportPoint(origin);
-        float x = Mathf.Abs(viewP.x - 0.5f);
-        if (x > 0.4f)
-        {
-            Debug.Log(true);
-        }
-
-
+        print(cam.transform.position);
+        float size = (float)(Mathf.Abs(max - min) * Screen.height / Screen.width * 0.5) + 3;
+        if (size < 5) size = 5;
+        cam.orthographicSize = size;
     }
+
 }
