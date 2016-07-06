488f112637c08a206e8bc53474e29758c56d5684
ScarletLeFairy <scarletlefairy@yahoo.de>
2016-07-06 02:23:18 +0200

    Added all the files Mike created for the Start and End scene but the start_menu_screen.psd file (which completely destroyed my repository that night). Do not add that huge files!

---
+++ Assets/Herbert/Scripts/Highscore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Highscore : MonoBehaviour {
+    // The default text to set
+    private static string DEFAULT_TEXT = "Highscore: ";
+
+    // The score
+    private int score;
+    // The text to set
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = DEFAULT_TEXT + score;
+	}
+
+    // Sets the highscore
+    void setHighscore(int score){
+        this.score = score;
+    }
+}
---
+++ Assets/Herbert/Scripts/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour { 
+    // The current application index
+    private int currentApplicationIndex;
+
+	// Use this for initialization
+	void Start () {
+        currentApplicationIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentApplicationIndex < 0) currentApplicationIndex = 0;
+
+	}
+
+    // Loads the next level
+    public void loadNextLevel() {
+        int nextApplicationIndex = currentApplicationIndex + 1;
+        if (nextApplicationIndex >= SceneManager.sceneCountInBuildSettings) nextApplicationIndex = 0;
+
+        SceneManager.LoadSceneAsync(nextApplicationIndex);
+    }
+}
